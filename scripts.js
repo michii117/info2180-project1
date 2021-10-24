bac5457ad61b71a4bf920d32b80852fba32ec70f
Demitri Grant <demitri.grant@gmail.com>
2021-10-24 15:49:15 -0500

    Edited script file with newsletter functionality

--- scripts.js
+++ scripts.js
@@ -1,1 +1,28 @@
 /* Add your JavaScript to this file */
+var email;
+
+
+
+window.addEventListener("load", (event) => {
+
+    var btn= document.getElementsByClassName("btn")[1];
+
+    btn.addEventListener("click", (event) => {
+        event.preventDefault();
+        email= document.querySelector('input[name="email"]').value;
+
+
+        if(email==""){
+            document.getElementsByClassName("message")[0].innerHTML="Please enter a valid email address.";
+
+        }else{
+            
+            var message= document.getElementsByClassName("message")[0];
+            document.getElementsByClassName("message")[0].innerHTML="Thank you! Your email address " + email + " has been added to our mailing list!";
+    
+        }
+        
+    })
+
+})
+
